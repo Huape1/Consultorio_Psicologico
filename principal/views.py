b5b60dff6eab4c1c9c8609f941a054a278e0322c
Huape1 <huape.gil.4om@gmail.com>
2026-03-12 17:18:12 -0700

    Agregar html

---
+++ principal/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def registro(request):
+    return render(request, 'registro.html')
+
+def especialidades(request):
+    return render(request, 'especialidades.html')
