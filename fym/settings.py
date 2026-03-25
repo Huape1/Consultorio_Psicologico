bf1aca35f4aa0418f82693ec2113dbad6b989a60
Huape1 <huape.gil.4om@gmail.com>
2026-03-24 17:46:48 -0700

    SQLite

--- fym/settings.py
+++ fym/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'fym.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresq1',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
